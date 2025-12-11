bcc52840968c4412b3a54065a9b10da174b36bc8
Sreenath V <sreenathvenugopal0465@gmail.com>
2025-12-11 19:34:30 +0530

    Solve LongestCommonPrefix problem

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String firstString = strs[0];
+        String commonPrefix = "";
+        for(int i=0;i<firstString.length();i++){
+            int flag=0;
+            char symbol = firstString.charAt(i);
+            for(int j=1;j<strs.length;j++){
+                String currentString = strs[j];
+                if(currentString.length()<=i || currentString.charAt(i)!=symbol){
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0){
+                commonPrefix += symbol;
+            }
+        }
+        if(commonPrefix.length()>0){
+            return commonPrefix;
+        }
+        else{
+            return "";
+        }
+    }
+}
