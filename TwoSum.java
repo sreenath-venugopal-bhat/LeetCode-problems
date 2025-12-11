0318239e9919f2eedac9d3463223c55b46840c50
Sreenath V <sreenathvenugopal0465@gmail.com>
2025-12-11 18:58:05 +0530

    Solve TwoSum problem

---
+++ TwoSum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] indices = new int[2];
+        int ptr=0;
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    indices[0]=i;
+                    indices[1]=j;
+                    break;
+                }
+            }
+        }
+        return indices;
+    }
+}
